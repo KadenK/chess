bd6155b66cf7a8f3ee0776d7afbdfa31c04c45a1
Kaden Keep <kaden.keep@gmail.com>
2024-02-18 18:08:07 -0700

    Initial project setup for phase 3 (phase3)

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import chess.*;
 
+import spark.Spark;
+
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
