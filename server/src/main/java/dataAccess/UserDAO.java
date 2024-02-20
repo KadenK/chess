594c0d4f33bed95efbc56cb25ce8edbbfa50938f
Kaden Keep <kaden.keep@gmail.com>
2024-02-20 14:44:26 -0700

    Implement UserDAO (phase3)

--- server/src/main/java/dataAccess/UserDAO.java
+++ server/src/main/java/dataAccess/UserDAO.java
@@ -1,4 +1,12 @@
 package dataAccess;
 
+import model.UserData;
+
 public interface UserDAO {
+
+    UserData getUser(String username);
+    void createUser(String username, String password, String email);
+    boolean authenticateUser(String username, String password);
+    void clear();
+
 }
