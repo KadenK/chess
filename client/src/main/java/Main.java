9e11722f1000b8304a830302130e1508a1e7b6a4
Kaden Keep <kaden.keep@gmail.com>
2024-03-17 21:30:43 -0600

    Add prelogin UI serverFacade methods (phase5)

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        ServerFacade server = new ServerFacade();
+
+        System.out.println(server.register("username", "password", "email"));
+
+        System.out.println(server.login("username", "password"));
     }
 }
