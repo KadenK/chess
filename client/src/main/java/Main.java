afd97b426067c656c859503762b0720d31431242
Kaden Keep <kaden.keep@gmail.com>
2024-03-20 19:32:39 -0600

    Minor cleanup (phase5)

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,31 +4,13 @@ import ui.PreloginREPL;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("♕ 240 Chess Client:");
 
         ServerFacade server = new ServerFacade();
 
         PreloginREPL prelogin = new PreloginREPL(server);
         prelogin.run();
         System.out.println("Exited");
-
-//        System.out.println(server.register("username", "password", "email"));
-//
-//        System.out.println(server.login("username", "password"));
-//
-//        System.out.println(server.logout());
-//
-//        int gameID = server.createGame("game1");
-//
-//        System.out.println(gameID);
-//
-//        System.out.println(server.listGames());
-//
-//        System.out.println(server.joinGame(gameID, "WHITE"));
-//
-//        System.out.println(server.listGames());
-
 
     }
 }
